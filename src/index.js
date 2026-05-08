f8321250e4907590d7219729ef00861f40209aa9
Leandro <prorootinfo@gmail.com>
2026-05-08 10:39:39 +0100

    Alterar projeto para estrutura Vite e alterar layout da aplicação

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ const app = express();
 
 app.use(express.json());
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Serve static frontend files from project root (index.html, public/)
 app.use(express.static(path.join(__dirname, '..')));
 
